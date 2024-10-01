ca386f7c0a350e49064084dc262fb1a30c729be3
TristanSalinas <salinas.tristan@gmail.com>
2024-10-01 14:47:05 +0000

    disconnect implementation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,20 @@
 const form = document.querySelector("form");
 const button = document.querySelector("button");
 const title = document.querySelector("#right-title");
+let isLogged = false;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
 button.addEventListener("click", () => {
-  title.textContent = "Bonjour venu d'index.js";
+  if (!isLogged){
+    title.textContent = "Bienvenue !";
+    button.textContent = "SE DECONNECTER"
+    isLogged = true;
+  } else {
+    title.textContent = "Préparez vous pour l'aventure";
+    button.textContent = "SE CONNECTER"
+    isLogged = false
+  }
 });
